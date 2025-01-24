39ce2f6b3a2e3c92fe1a51eb6ba79ea59fb1e8ad
Ruben Steins <r.steins@fontys.n>
2025-01-24 21:34:37 +0100

    Added Area3D bounding box

--- Scripts/fake_player.gd
+++ Scripts/fake_player.gd
@@ -3,6 +3,9 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+var canMoveLeft : bool = true
+var canMoveRight : bool = true
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -14,16 +17,28 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	if ((Input.is_action_pressed("ui_left") and canMoveLeft) or (Input.is_action_pressed("ui_right") and canMoveRight)):
+		var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+		var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
+		canMoveLeft = true
+		canMoveRight = true
 
-	move_and_slide()
+		move_and_slide()
 
 func _on_bouding_box_body_exited(body: Node3D) -> void:
 	print_debug("Exited")
+	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction == Vector3.LEFT:
+		# no more left for you.
+		canMoveLeft = false
+		canMoveRight = true
+	else:
+		canMoveRight = false
+		canMoveLeft = true
