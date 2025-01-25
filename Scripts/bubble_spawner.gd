6aae802c2360440ccfcb30424232f581feb5e8d1
Ruben Steins <r.steins@fontys.n>
2025-01-25 13:53:23 +0100

    Bubbles that give oxygen spawn automatically evey spawn-interval

---
+++ Scripts/bubble_spawner.gd
@@ -0,0 +1,16 @@
+extends Marker3D
+
+@export var nr_of_bubbles : int = 3
+@export var spawn_interval : int = 3
+
+var bubble_seen = preload("res://Environment/bubble.tscn")
+		
+func spawn_bubble() -> void:
+	var bubble = bubble_seen.instantiate()
+	var x = -5 + (randf() * 10.0)
+	printt(x, self.position.y, self.position.z)
+	bubble.position = Vector3(x, position.y, position.z)	
+	add_child(bubble)
+
+func _on_timer_timeout() -> void:
+	spawn_bubble()
