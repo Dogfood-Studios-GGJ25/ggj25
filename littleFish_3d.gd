ecbc2a10c149ae42d9c7c321223ad7478cec3465
Erwin <erwin.bell@fontys.nl>
2025-01-25 16:48:57 +0100

    visjes toegevoegd

--- littleFish_3d.gd
+++ littleFish_3d.gd
@@ -1,10 +1,10 @@
+
 extends Sprite3D
-var speed = 0.3
+var speed = 0.003
 var angle = -PI
 var time = 0
 var swimming = false
 var swimtime = 0.0
-
 
 
 # Called when the node enters the scene tree for the first time.
@@ -15,24 +15,22 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	swim(delta)
-	pass
 	
 func swim(delta):
 
 	if !swimming: 
 		swimtime = randf_range(0.1,0.5) * 10
-		rotation += randf() * PI
+		#rotation += randf() * PI
+		#rotate_z(speed)
 		swimming = true
 
 	if swimming:
 		time += delta
-		print_debug("swimtime: ", swimtime)
-		print_debug("time: ", time)
 		if time <= swimtime:
-			var velocity = Vector3.LEFT.rotated(rotation) * speed
-			position += velocity
+			#var velocity = Vector3.LEFT.rotated(rotation) * speed
+			rotate_z(speed)
+			#transform.basis.x *= speed
+			#position += velocity
 		else:
 			time = 0
 			swimming = false
-pass
-	
