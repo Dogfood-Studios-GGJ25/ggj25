f1b1465e11610980e943cad19acbaca87fe58888
Dennis <d.kirsch@fontys.nl>
2025-01-25 04:12:36 +0100

    Created little bubbly guy. :) It's 4:12am help

--- Scripts/spot_light_3d.gd
+++ Scripts/spot_light_3d.gd
@@ -13,7 +13,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	check_all_raycasts()
+	if visible:
+		check_all_raycasts()
 
 
 func check_all_raycasts():
