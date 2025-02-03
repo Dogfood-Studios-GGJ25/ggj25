388aad71bc958fd60e197ee75d529679223d9c80
Ruben Steins <rubensteins@gmail.com>
2025-02-03 22:04:40 +0100

    Bubbles are now 'top level', move independantly of 'elevator' #26

--- Scripts/bubble_spawner.gd
+++ Scripts/bubble_spawner.gd
@@ -5,13 +5,10 @@ extends Marker3D
 func spawn_bubble() -> void:
 	var bubble = bubble_seen.instantiate()
 	var x = -4 + (randf() * 8.0)
-	#bubble.position = Vector3(x, position.y, position.z)	
-	#add_child(bubble)
 	
 	# Detach the bubble and reparent it to a different node
 	bubble.global_transform = global_transform  # Preserve global position
 	get_parent().get_parent().add_child(bubble)  # Reparent to the camera's parent or another node
-	#remove_child(bubble)  # Detach from the current parent
 	bubble.position.x += x
 
 func _on_timer_timeout() -> void:
