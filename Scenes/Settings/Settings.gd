e7c3d0976a0924c2c165915b6200a9f7e44381f1
Ruben Steins <rubensteins@gmail.com>
2025-02-21 12:45:18 +0100

    Added controls to Settings panel. Opens and closes as expected. No func yet.

--- Scenes/Settings/Settings.gd
+++ Scenes/Settings/Settings.gd
@@ -3,4 +3,11 @@ extends TextureButton
 @onready var settings_panel : Panel = $SettingsPanel
 
 func _on_pressed() -> void:
-	pass # Replace with function body.
+	if not settings_panel.visible:
+		settings_panel.show()
+		
+func _on_close_button_pressed() -> void:
+	# store all settings
+	
+	#close the panel
+	settings_panel.hide()
